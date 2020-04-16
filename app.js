b6e2adbcd53c7e6237b274367bfc8393e2c4e1d4
pragya-neural <63223531+pragya-neural@users.noreply.github.com>
2020-04-16 17:14:35 +0530

    prg commit 4

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var propertyRouter = require('./routes/property');
+var adminRouter = require('./routes/admin');
 
 
 var app = express();
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/property', propertyRouter);
+app.use('/admin', adminRouter);
 
  // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
-var curd_module = require('./crud_module');
+var curd_module = require('./curd_module');
 
-router.get('/home', checkLogin,function(req, res, next) {
+router.get('/home', function(req, res, next) {
     res.render('admin/home', { title: 'Nobroker Admin-Panel'});
   });
 
-  
+
 
 module.exports = router;
