8d73f7fbedadacd8a18dc87db86e21b71c536f57
pragya-neural <63223531+pragya-neural@users.noreply.github.com>
2020-04-17 14:27:11 +0530

    prg commit 17/4

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,28 @@ router.get('/home', function(req, res, next) {
     res.render('admin/home', { title: 'Nobroker Admin-Panel'});
   });
 
-
+  router.get('/userreport', function(req, res, next) {
+    var where = "active_status is not null";
+    curd_module.all_data_select('user_id,user_login,name,email_id,mobile_no,user_type,active_status','users',where,'user_id desc',function(user_detail){
+      var obj = {};
+      obj.user_detail = user_detail;
+     
+    res.render('admin/userreport', { title: 'Nobroker Users Reports', obj:obj });
+  
+  });
+});
+/*
+  router.get('/',checkLogin,function(req, res, next) {
+    var where = "active_status='active'";
+    curd_module.all_data_select('property_type_id,property_type','property_type',where,'property_type_id desc',function(pro_type){
+    var where = "active_status='active'";
+    curd_module.all_data_select('property_purpose_id,property_purpose_name','property_purpose',where,'property_purpose_id desc',function(purpose){
+      var obj = {};
+      obj.purpose = purpose;
+      obj.pro_type = pro_type;
+      res.render('property', { title: 'Property Entry', sideselection: 'property',obj:obj });
+    });
+   });
+  }); */
 
 module.exports = router;
